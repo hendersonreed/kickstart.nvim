4def1a1f1d89c8ea1c43058f3dccd1d514d7fbd6
Henderson Reed Hummel <hhummel@digitalocean.com>
2024-11-19 15:21:42 -0800

    added lazydev for nvim completions.

--- init.lua
+++ init.lua
@@ -121,7 +121,18 @@ require('lazy').setup {
       },
     },
   },
-
+  {
+    -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+    -- used for completion, annotations and signatures of Neovim apis
+    'folke/lazydev.nvim',
+    ft = 'lua',
+    opts = {
+      library = {
+        -- Load luvit types when the `vim.uv` word is found
+        { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+      },
+    },
+  },
   {
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
