c2f8b11e538ab064708fa9257321ec85e4dca602
Henderson Reed Hummel <hhummel@digitalocean.com>
2024-11-19 15:27:14 -0800

    add mappings for gitsigns.

--- init.lua
+++ init.lua
@@ -507,5 +507,63 @@ require('lazy').setup {
   },
 }
 
+require('gitsigns').setup {
+  on_attach = function(bufnr)
+    local gitsigns = require 'gitsigns'
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      if opts.desc then
+        opts.desc = 'Gitsigns: ' .. opts.desc
+      end
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    -- Navigation
+    map('n', ']c', function()
+      if vim.wo.diff then
+        vim.cmd.normal { ']c', bang = true }
+      else
+        gitsigns.nav_hunk 'next'
+      end
+    end)
+
+    map('n', '[c', function()
+      if vim.wo.diff then
+        vim.cmd.normal { '[c', bang = true }
+      else
+        gitsigns.nav_hunk 'prev'
+      end
+    end)
+
+    -- Actions
+    map('n', '<leader>hs', gitsigns.stage_hunk, { desc = 'stage hunk' })
+    map('n', '<leader>hr', gitsigns.reset_hunk, { desc = 'reset hunk' })
+    map('v', '<leader>hs', function()
+      gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
+    end, { desc = 'stage hunk (line)' })
+    map('v', '<leader>hr', function()
+      gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
+    end, { desc = 'reset hunk (line)' })
+    map('n', '<leader>hS', gitsigns.stage_buffer, { desc = 'stage buffer' })
+    map('n', '<leader>hu', gitsigns.undo_stage_hunk, { desc = 'undo stage hunk' })
+    map('n', '<leader>hR', gitsigns.reset_buffer, { desc = 'reset buffer' })
+    map('n', '<leader>hp', gitsigns.preview_hunk, { desc = 'preview hunk' })
+    map('n', '<leader>hb', function()
+      gitsigns.blame_line { full = true }
+    end, { desc = 'blame' })
+    map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = 'toggle blame (line)' })
+    map('n', '<leader>hd', gitsigns.diffthis, { desc = 'diff this' })
+    map('n', '<leader>hD', function()
+      gitsigns.diffthis '~'
+    end, { desc = 'diff this (line)' })
+    map('n', '<leader>td', gitsigns.toggle_deleted, { desc = 'toggle deleted' })
+
+    -- Text object
+    map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = 'select hunk' })
+  end,
+}
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
